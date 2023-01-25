990406f2883bc0e3cc8d3a6390b1073881a60dd9
miladbeigi <milad.be@gmail.com>
2023-01-25 14:32:02 +0100

    add model params

--- models/baseline_constants.py
+++ models/baseline_constants.py
@@ -17,6 +17,8 @@ MAIN_PARAMS = {
 MODEL_PARAMS = {
     'cifar100.cnn': (0.01, 100),
     'cifar10.cnn': (0.01, 10),
+    # Number of blocks (list), number of classes
+    'cifar10.resnet20' : (0.01, [3, 3, 3] , 10)
 }
 """dict: Model specific parameter specification"""
 
